291bf0bd5d0f79da40e298a49306f87afb5f6bf0
David Basile Filho <davidbasilefilho@gmail.com>
2023-05-15 10:47:59 -0300

    added a blank space at the end of the container

--- src/App.jsx
+++ src/App.jsx
@@ -53,8 +53,8 @@ export default function App() {
         <>
             <MainNavbar></MainNavbar>
 
-            <Container>
-                <Card className="shadow bg-dark-subtle border-0 p-3 mt-3">
+            <Container className="my-3">
+                <Card className="shadow bg-dark-subtle border-0 p-3">
                     <h2 className="text-center mb-4">React Todo List</h2>
 
                     <TodoForm onSubmit={addTodo}></TodoForm>
