2c26c33064f9b95804e1188848dd81cb054d19d9
David Basile Filho <davidbasilefilho@gmail.com>
2023-05-15 10:31:41 -0300

    forgot to return the jsx code in the todo list element, now it works perfectly

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -5,21 +5,23 @@ import { Container } from "react-bootstrap";
 import { TodoItem } from "./TodoItem";
 
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
-    <Container fluid>
-        <h3>Todo list</h3>
+    return (
+        <Container fluid>
+            <h3>Todo list</h3>
 
-        <ul className="px-4 w-100">
-            {todos.length === 0 && "No todos"}
-            {todos.map((todo) => {
-                return (
-                    <TodoItem
-                        {...todo}
-                        key={todo.id}
-                        toggleTodo={toggleTodo}
-                        deleteTodo={deleteTodo}
-                    ></TodoItem>
-                );
-            })}
-        </ul>
-    </Container>;
+            <ul className="px-4 w-100">
+                {todos.length === 0 && "No todos"}
+                {todos.map((todo) => {
+                    return (
+                        <TodoItem
+                            {...todo}
+                            key={todo.id}
+                            toggleTodo={toggleTodo}
+                            deleteTodo={deleteTodo}
+                        ></TodoItem>
+                    );
+                })}
+            </ul>
+        </Container>
+    );
 }
